071e01d79ccb3487e64e37c1fb2aae0b4ceb7d22
Sean Johnson <pirogoeth@maio.me>
2015-08-14 14:59:47 -0500

    0.0.11: No more rackup config, add namespacing to app.rb

--- lib/redmon/app.rb
+++ lib/redmon/app.rb
@@ -19,62 +19,71 @@ module Redmon
       :cache_control => 'public, max-age=3600'
     }
 
-    get '/' do
-      protected!
-      haml :app
+    if Redmon.config.base_path.end_with? '/'
+      base_path = Redmon.config.base_path.slice(0, base_path.length - 1)
+    else
+      base_path = Redmon.config.base_path
     end
 
-    get '/health/app' do
-      'okay'
-    end
+    base_path.strip!
 
-    get '/health/redis' do
-      begin
-        @result = redis.send "ping"
-        @result = empty_result if @result == []
-        if @result != "PONG"
+    namespace base_path do
+      get '/' do
+        protected!
+        haml :app
+      end
+
+      get '/health/app' do
+        'okay'
+      end
+
+      get '/health/redis' do
+        begin
+          @result = redis.send "ping"
+          @result = empty_result if @result == []
+          if @result != "PONG"
+            "not okay"
+          else
+            "okay"
+          end
+        rescue ::Redis::CannotConnectError
           "not okay"
-        else
-          "okay"
+        rescue
+          "unknown"
         end
-      rescue ::Redis::CannotConnectError
-        "not okay"
-      rescue
-        "unknown"
+      end
+
+      get '/cli' do
+        protected!
+        args = params[:command].split(/ *"(.*?)" *| *'(.*?)' *| /)
+        args.reject!(&:empty?)
+        @cmd = args.shift.downcase.intern
+        begin
+          raise RuntimeError unless supported? @cmd
+          @result = redis.send @cmd, *args
+          @result = empty_result if @result == []
+          haml :cli
+        rescue ArgumentError
+          wrong_number_of_arguments_for @cmd
+        rescue RuntimeError
+          unknown @cmd
+        rescue Errno::ECONNREFUSED
+          connection_refused
+        end
+      end
+
+      post '/config' do
+        protected!
+        param = params[:param].intern
+        value = params[:value]
+        redis.config(:set, param, value) and value
+      end
+
+      get '/stats' do
+        protected!
+        content_type :json
+        redis.zrange(stats_key, count, -1).to_json
       end
     end
-
-    get '/cli' do
-      protected!
-      args = params[:command].split(/ *"(.*?)" *| *'(.*?)' *| /)
-      args.reject!(&:empty?)
-      @cmd = args.shift.downcase.intern
-      begin
-        raise RuntimeError unless supported? @cmd
-        @result = redis.send @cmd, *args
-        @result = empty_result if @result == []
-        haml :cli
-      rescue ArgumentError
-        wrong_number_of_arguments_for @cmd
-      rescue RuntimeError
-        unknown @cmd
-      rescue Errno::ECONNREFUSED
-        connection_refused
-      end
-    end
-
-    post '/config' do
-      protected!
-      param = params[:param].intern
-      value = params[:value]
-      redis.config(:set, param, value) and value
-    end
-
-    get '/stats' do
-      protected!
-      content_type :json
-      redis.zrange(stats_key, count, -1).to_json
-    end
-
   end
 end
